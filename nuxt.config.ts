9513495c7ca25ba2433d8fc2f558346a6bed7df3
ithiel883 <ajuka.zephiniah@gmail.com>
2023-10-03 14:09:05 +0100

    setup: layout for app

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
   srcDir: 'src/',
-  modules: ['@nuxt/ui']
+  alias: {
+    'class-validator': 'class-validator/cjs/index.js',
+  },
+  modules: ['@nuxt/ui'],
+  pages: true
 })
---
+++ src/pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        something
+    </div>
+</template>
+  
