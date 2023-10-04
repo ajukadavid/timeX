c289caaabfab8852ad8cdcffe0e1ad4aba2f70dc
ithiel883 <ajuka.zephiniah@gmail.com>
2023-10-04 10:26:15 +0100

    addition: axios interceptors

--- src/composables/helpers/axios-interceptor.ts
+++ src/composables/helpers/axios-interceptor.ts
@@ -6,11 +6,88 @@ import {
     AxiosResponse,
   } from "axios";
   
-  
+  const BASE_URL = 'https://timex-vzwo.onrender.com/api/v1'
   // For Make Log on Develop Mode
   const logOnDev = (message: string) => {
     if (import.meta.env.MODE === "development") {
       console.log(message);
     }
+  };
+
+
+  // Request Interceptor
+export const onRequest = (config: InternalAxiosRequestConfig): InternalAxiosRequestConfig => {
+    const { method, url } = config;
+    config.url = BASE_URL + url
+    logOnDev(`🚀 [API] ${method?.toUpperCase()} ${url} | Request`);
+  
+    if (method === "get") {
+      config.timeout = 15000;
+    }
+    return config;
+  };
+  
+  const onResponse = (response: AxiosResponse): AxiosResponse => {
+    const { method, url } = response.config;
+    const { status } = response;
+    // Set Loading End Here
+    // Handle Response Data Here
+    // Error Handling When Return Success with Error Code Here
+    logOnDev(`🚀 [API] ${method?.toUpperCase()} ${url} | Response ${status}`);
+    console.log(response)
+    return response;
+  };
+  
+
+  export  const onErrorResponse = (error: AxiosError | Error): Promise<AxiosError> => {
+    if (axios.isAxiosError(error)) {
+      const { message } = error;
+      const { method, url } = error.config as AxiosRequestConfig;
+      const { statusText, status } = error.response as AxiosResponse ?? {};
+  
+      logOnDev(
+        `🚨 [API] ${method?.toUpperCase()} ${url} | Error ${status} ${message}`
+      );
+  
+      switch (status) {
+        case 401: {
+          // "Login required"
+          break;
+        }
+        case 403: {
+          // "Permission denied"
+          break;
+        }
+        case 404: {
+          // "Invalid request"
+          break;
+        }
+        case 500: {
+          // "Server error"
+          break;
+        }
+        default: {
+          // "Unknown error occurred"
+          break;
+        }
+      }
+  
+      if (status === 401) {
+        // Delete Token & Go To Login Page if you required.
+        sessionStorage.removeItem("token");
+      }
+    } else {
+      logOnDev(`🚨 [API] | Error ${error.message}`);
+    }
+  
+    return Promise.reject(error);
+  };
+  
+
+  export const setupInterceptors = (instance: AxiosInstance): AxiosInstance => {
+    instance.interceptors.request.use(onRequest, onErrorResponse);
+    instance.interceptors.response.use(onResponse, onErrorResponse);
+  
+    return instance;
   };
   
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,33 @@
 <script setup>
 const colorMode = useColorMode()
+import axios from 'axios'
+import { setupInterceptors } from '../composables/helpers/axios-interceptor'
+
+const axiosInstance = setupInterceptors(axios.create());
+
+async function fetchData() {
+
+    const data = {
+        firstName: 'david',
+        lastName: 'ajuka',
+        email: 'ajuka@ajuka.com',
+        companyName: 'ajukscompany',
+        phone: 344421144,
+        password: 'lepass'
+    }
+
+    try {
+        const response = await axiosInstance.post('/employers', data);
+        // Handle the response here
+        console.log(response)
+    } catch (error) {
+        // Handle errors here
+    }
+}
+
+onMounted(() => {
+    fetchData()
+})
 
 const isDark = computed({
     get() {
