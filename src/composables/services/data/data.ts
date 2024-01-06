6e9da7d076fc89269f7fb0a4df9a8f6022e43fa1
ithiel883 <ajuka.zephiniah@gmail.com>
2024-01-06 19:37:49 +0100

    create and manage department flow and integration

--- src/composables/services/data/data.ts
+++ src/composables/services/data/data.ts
@@ -14,7 +14,22 @@ export const getStaffs = async (page?: number) => {
   return response.data;
 };
 
+export const getDepartments = async (page?: number) => {
+  let response;
+  if (page) {
+    response = await axiosInstance.get(`/departments?page=${page}`);
+  } else {
+    response = await axiosInstance.get(`/departments`);
+  }
+  return response.data;
+};
 export const registerStaff = async (data: StaffRegister) => {
   const response = await axiosInstance.post("/staffs", data);
   return response.data;
+};
+
+export const createDepartment = async (name: string) => {
+  console.log(name);
+  // const response = await axiosInstance.post("/departments", { name });
+  // return response.data;
 };
