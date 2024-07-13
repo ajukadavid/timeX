1826947fcaf221ec39eb54e682b51805286880c2
ithiel883 <ajuka.zephiniah@gmail.com>
2024-07-13 15:29:50 +0100

    added landing splash page

--- src/layouts/landing.vue
+++ src/layouts/landing.vue
@@ -1,7 +1,18 @@
 <template>
-  <div>
-    this is landing
-
-    <slot />
+  <div class="landing h-screen bg-cover bg-center bg-no-repeat">
+    <div class="flex justify-between items-center mx-4">
+      <img src="/logo.png" class="w-16 md:w-28 cursor-pointer" />
+      <div class="flex space-x-3 font-bold">
+        <span class="cursor-pointer">Contact Us</span>
+        <span class="cursor-pointer">Sign up</span>
+        <span class="cursor-pointer">Log in</span>
+      </div>
+    </div>
   </div>
 </template>
+
+<style scoped>
+.landing {
+  background-image: url("/landing_splash.jpg");
+}
+</style>
