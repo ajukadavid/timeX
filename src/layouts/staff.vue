4210541a0c5189966cd909089a2040b1836d06e2
Ajuka David <ajuka.zephiniah@gmail.com>
2024-09-06 13:07:05 +0100

    setup sign in for staff dashboard

--- src/layouts/staff.vue
+++ src/layouts/staff.vue
@@ -1,10 +1,24 @@
 <script setup lang="ts">
 import { storeToRefs } from "pinia";
 import { useUserStore } from "@/store/userStore";
+import { getStaff } from '@/composables/services/data/data';
+
 
 const store = useUserStore();
+const $route = useRoute();
 
 const { name, role } = storeToRefs(store);
+
+const staffDetails = {
+  firstName: "",
+  lastName: "",
+}
+
+onMounted( async () => {
+  const staff = await getStaff($route.params.id);
+  staffDetails.firstName = staff.staff.firstName
+  staffDetails.lastName = staff.staff.lastName
+});
 </script>
 <template>
   <div class="flex bg-white h-screen dark:bg-primary-800 flex-col">
@@ -13,7 +27,7 @@ const { name, role } = storeToRefs(store);
         <img src="/logo.png" class="w-16 md:w-28 cursor-pointer" />
       </div>
       <div class="flex flex-col px-10 pt-10">
-        <span class="text-2xl font-bold">Welcome, {{ name }}</span>
+        <span class="text-2xl font-bold">Hello, {{ store.name }}</span>
         <span class="text-sm font-light">Here is the breakdown of your sign in time</span>
       </div>
     </div>
--- src/pages/dashboardStaff/[id]/index.vue
+++ src/pages/dashboardStaff/[id]/index.vue
@@ -1,18 +1,30 @@
 <script lang="ts" setup>
+import { getStaff } from '@/composables/services/data/data';
+import { useUserStore } from '@/store/userStore';
 
-
+const store = useUserStore();
+const $route = useRoute();
 definePageMeta({
   layout: 'staff'
 })
 
+const staff = {
+  firstName: "",
+  lastName: "",
+}
 
-onMounted(() => {});
+onMounted( async () => {
+  const staffData = await getStaff($route.params.id);
+   store.$patch({
+      role: staffData.staff.role,
+      name: `${staffData.staff.firstName} ${staffData.staff.lastName}`,
+    });
+});
 </script>
 
 <template>
   <main class="p-10">
     <div class="flex items-center justify-between">
-     le table
     </div>
     
   </main>
